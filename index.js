56324179e598e7a14d2e7769af9ddf2aa952b161
BookmDan <danoh329@gmail.com>
2023-07-01 16:23:50 -0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = new Array("Milo", "Otis", "Garfield");
+
+function destructivelyAppendCat(name) {
+  const newcats = [...cats];
+  newcats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function appendCat(name) {
+  cats = [...cats]
+  cats.push(name);
+}
+
+function prependCat(name) {
+  catsnew = [...cats]
+  catsnew.unshift(name);
+}
+
+function removeLastCat() {
+  catsnew = [...cats]
+  catsnew.pop();
+}
